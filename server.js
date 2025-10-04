2129f4dd858d66c2ca413b77d47f8237606ec7ba
Hyrummorales23 <hyrummorales23@gmail.com>
2025-10-04 14:48:05 -0600

    cors update

--- server.js
+++ server.js
@@ -7,16 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // CORS Middleware
-app.use(cors({
-  origin: [
-    'https://trackstar-api.onrender.com',
-    'http://localhost:3000',
-    'https://localhost:3000'
-  ],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Accept']
-}));
+app.use(cors());
 
 // Middleware
 app.use(express.json());
